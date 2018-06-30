c31a8d87cfa8942ccbf632be4543d9998842fbde
ivan koop <ivankoop3@gmail.com>
2018-06-29 20:40:33 -0400

    first commit

---
+++ src/components/Search/index.js
@@ -0,0 +1,12 @@
+import style from "./Search.css"
+import React from "react";
+
+export default class Search extends React.Component {
+    render(){
+      return (
+        <div className={style.holis}>
+                 Hello Search Component :)
+               </div>
+      );
+    }
+}
---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Search from './components/Search'
+
+const Index = () => {
+  return (
+    <div>
+      <Search>
+
+      </Search>
+    </div>
+  )
+};
+
+
+
+
+ReactDOM.render(<Index />, document.getElementById("index"));
